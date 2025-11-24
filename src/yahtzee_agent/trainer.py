d622eae59731b786f40ecc508461b75c64f002eb
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2025-11-24 03:11:23 -0600

    pos weight

--- src/yahtzee_agent/trainer.py
+++ src/yahtzee_agent/trainer.py
@@ -430,9 +430,12 @@ class YahtzeeAgentTrainer(lightning.LightningModule):
         )
 
         if self.algorithm == Algorithm.A2C:
+            pos_weight = torch.tensor(
+                [10.0], device=bonus_likelihood_logit.device
+            )  # e.g., k = 5 or 10
             bonus_likelihood_loss_weight = 20.0  # Smaller than critic loss
             bonus_likelihood_loss = torch.nn.functional.binary_cross_entropy_with_logits(
-                bonus_likelihood_logit.squeeze(), received_bonus.float()
+                bonus_likelihood_logit.squeeze(), received_bonus.float(), pos_weight=pos_weight
             )
             loss += bonus_likelihood_loss_weight * bonus_likelihood_loss
             self.log(
