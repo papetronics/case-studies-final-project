5504aaf96ce686f5efc1a31a45e73274c7d72a05
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2025-11-25 09:16:50 -0600

    Fix Ngs formula

--- src/yahtzee_agent/trainer.py
+++ src/yahtzee_agent/trainer.py
@@ -560,8 +560,8 @@ class YahtzeeAgentTrainer(lightning.LightningModule):
         next_potential = (next_score / MINIMUM_UPPER_SCORE_FOR_BONUS) * BONUS_POINTS
         current_potential = (current_score / MINIMUM_UPPER_SCORE_FOR_BONUS) * BONUS_POINTS
 
-        # Calculate potential difference
-        potential_diff = next_potential - current_potential
+        # Calculate potential difference using Ng's formula
+        potential_diff = self.get_gamma() * next_potential - current_potential
         shaping_bonus = self.upper_score_shaping_weight * potential_diff
 
         # Shaped reward = r + weight * (Phi(s') - Phi(s))
